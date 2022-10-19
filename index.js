0eee3e057d31527cdce7757b4bee9375542b75a7
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2022-10-20 02:03:29 +0600

    Added New Routes to load data.

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use(cors());
 const port = process.env.PORT || 5000;
 
 const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
 app.get('/', (req, res) => {
 	res.send('News API is running');
@@ -14,6 +15,26 @@ app.get('/', (req, res) => {
 
 app.get('/news-categories', (req, res) => {
 	res.send(categories);
+});
+
+app.get('/category/:id', (req, res) => {
+	const { id } = req.params;
+	if (id === '08') {
+		res.send(news);
+	} else {
+		const selectedCategory = news.filter((news) => news.category_id === id);
+		res.send(selectedCategory);
+	}
+});
+
+app.get('/allNews', (req, res) => {
+	res.send(news);
+});
+
+app.get('/news/:id', (req, res) => {
+	const { id } = req.params;
+	const selectedNews = news.find((n) => n._id === id);
+	res.send(selectedNews);
 });
 
 app.listen(port, () => {
